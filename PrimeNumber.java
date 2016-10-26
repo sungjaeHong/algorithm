2e07d39f79f29ee4217e01d7d8632c6bfe0cece9
sungjaeHong <sungjae89@gmail.com>
2016-10-26 11:22:23 +0900

    로직 업데이트
    www.hackerrank.com 에서 run했을 때 timeout으로 테스트를 통과 못했다.
    이유를 찾아보니 비교값이 클 경우, 예를들어 1000000000, 1000000001, 1000000002, 1000000003 같은 값을 비교할때 너무 많은 loop를 돈다.
    따라서 로직 업데이트를 해야할 필요성을 느꼈고, 비교할 값의 루트값을 찾아내 그 이하로만 비교하였다. 어짜피 루트숫자와 나머지가 0이가 떨어진다면 소수가 아니기에...

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,28 +1,35 @@
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
+import java.util.Scanner;
 
 public class PrimeNumber {
     public static void main(String[] args) {
         int inputCnt = 0;
         Scanner scan = new Scanner(System.in);
         inputCnt = scan.nextInt();
-        int[] checkNumber = new int[inputCnt];
-        for(int i=0;i<checkNumber.length;i++){
-            checkNumber[i] = scan.nextInt();
+        if(inputCnt>0){	//음수에 대한 방어코드
+            int[] checkNumber = new int[inputCnt];
+            for(int i=0;i<checkNumber.length;i++){
+                int inputNumber = scan.nextInt();
+                if(inputNumber<=0){
+                	System.out.println("양의 정수만 입력하세요"); //음수에 대한 방어코드
+                	break;
+                }
+                checkNumber[i] = inputNumber;
+            }
+            long start = System.currentTimeMillis();
+            isPrimeNumber(checkNumber);
+            long end = System.currentTimeMillis();
+            System.out.println("실행 시간 : " + (double)(end-start) + "(ms)");
         }
-        isPrimeNumber(checkNumber);
     }
     
     public static void isPrimeNumber(int[] number){
-        boolean isPrime = true;
+        
         for(int i=0;i<number.length;i++){
-            if(number[i] == 1 || number[i] ==2){
+        	boolean isPrime = false;
+            if(number[i] ==2){  //생각해보니 1은 소수가 아님;;
                 isPrime = true;
-            } else{
-                for(int j=2;j<number[i];j++){
+            } else if(number[i] != 1){
+                for(int j=2;j<=Math.sqrt(number[i]);j++){    //  1000000001값에 타임아웃이 걸려서 로직업그레이드
                     if(number[i]%j==0){
                         isPrime = false;
                         break;
